fe36ffc9329da34bfce3547a5c2fb0e66f535462
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2022-05-20 13:11:40 +0700

    Haryono Kudadiri
    Rinaeatih Lingga

---
+++ includes/_footer.php
@@ -0,0 +1,16 @@
+<footer>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; ◇ HARY-IT</p>
+                </div>
+            </div>
+        </footer>
+ </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/myscript.js"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3703361972651186"
+     crossorigin="anonymous"></script>
+  </body>
+</html>
